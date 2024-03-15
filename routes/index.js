067fd9c063f3731992853a9619be4bfcef19004d
chandan viswakarma <chandanvishw11002@gmail.com>
2024-03-15 22:07:22 +0530

    mongoose

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,44 @@ var router = express.Router();
 const userModel = require('./users');
 const passport = require('passport');
 const upload = require("./multer");
+const postModel = require('./post');
+
 const localStrategy = require('passport-local');
 passport.use(new localStrategy(userModel.authenticate()));
 
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {nav:false});
 });
 
 router.get('/register', function(req,res,next){
- res.render('register');
+  res.render('register',{nav:false});
+});
+
+router.get('/add', function(req,res,next){
+  res.render('add', {nav:true});
+});
+
+router.post('/createPost', isLoggedIn, upload.single('postImage'), async function(req,res,next){
+  const user = await userModel.findOne({username : req.session.passport.user});
+  const post=await postModel.create({
+    user: user._id,
+    title: req.body.title,
+    description: req.body.description,
+    image: req.file.filename
+  })
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect('/profile');
 });
 
 router.get('/profile', isLoggedIn, async function(req,res,next){
+  const user = await userModel.findOne({username : req.session.passport.user}).populate("posts");
+  res.render('profile', {user, nav:true})
+});
+
+router.post('/createPost', isLoggedIn,upload.single("postImage"), async function(req,res,next){
   const user = await userModel.findOne({username : req.session.passport.user});
-  res.render('profile', {user})
+  res.render('profile', {user, nav:true})
 });
 
 router.post('/uploadfile', isLoggedIn, upload.single('file'), async function(req,res,next){
@@ -26,7 +50,6 @@ router.post('/uploadfile', isLoggedIn, upload.single('file'), async function(req
   res.redirect("/profile");
 });
 
- 
 router.post('/register', function(req,res,next){
     const user = new userModel({
       username: req.body.username,
